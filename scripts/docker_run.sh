95a919e850f5ae0dd69026f37478f4610c9cf83b
Parth Pratim Chatterjee <parth27official@gmail.com>
2025-04-23 09:10:39 +0000

    Adding docker build scripts
    Signed-off-by: Parth Pratim Chatterjee <parth27official@gmail.com>

---
+++ scripts/docker_run.sh
@@ -0,0 +1,8 @@
+export HUB_TOKEN=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
+export WANDB_API_KEY=74e8368d36e7abf19bc9d56a66e74a7bea1033e5
+if [[ -n "$1" ]]; then
+	docker run  -it --entrypoint /bin/bash --name $1 --gpus all  -v .:/workspace   parthpratim27/smart-repmatch 
+else
+	docker run  -it --entrypoint /bin/bash --gpus all  -v .:/workspace  parthpratim27/smart-repmatch 
+fi
+
